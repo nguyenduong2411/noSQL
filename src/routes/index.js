1056ce14b061b6ad18cd65ecc7b1949055ec53c3
nguyenduong2411 <nguyenduongkg2411@gmail.com>
2024-05-21 23:48:41 +0700

    chane

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,24 +1,38 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-const ProductController = require('../controller/product.controller')
-router.use('/api/v1/product', require('./product'));
+//const ProductController = require('../controller/product.controller')
+//router.use('/api/v1/product', require('./product'));
+const ProductModel = require('../models/product.model');
 
 router.get('/', async(req, res) => {
-    const indexView = path.join(__dirname, '../views/index.ejs')
-    const product = await ProductController.getListProduct();
-    res.render(indexView, { data: product });
+    res.render('views/index');
 });
+router.post('/submit', (req, res) => {
+    const data = req.body || {}
+    try {
+        ProductModel.insertMany({
+            ten: data.name,
+            gioitinh: data.gioitinh,
+            tuoi: data.tuoi,
+            diemToan: data.toan,
+            diemLy: data.ly,
+            diemHoa: data.hoa,
+        })
+    } catch (e) {
+        throw new Error("Missing")
+    } finally {
+        res.redirect("/admin")
+    }
+})
 
-router.get('/detail/:id', async(req, res) => {
-    const id = req.params.id;
-    const indexView = path.join(__dirname, '../views/detail.ejs')
-    const product = await ProductController.getProduct(id);
-    res.render(indexView, { product });
-});
-
-router.get('/admin', async(req, res) => {
-    const indexView = path.join(__dirname, '../views/admin.ejs')
-    res.render(indexView);
+router.get('/view', async(req, res) => {
+    try {
+        const data = await ProductModel.find().exec();
+        res.render('views/index', { data });
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).send('Internal server error');
+    }
 });
 module.exports = router;
--- src/routes/product/index.js
+++ src/routes/product/index.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const router = express.Router();
-const ProductModel = require('../../models/product.model')
-const ProductController = require('../../controller/product.controller')
-router.get('/', (req, res) => {
-    res.send('Product router');
-});
+// const express = require('express');
+// const router = express.Router();
+// const ProductModel = require('../../models/product.model')
+// const ProductController = require('../../controller/product.controller')
+// router.get('/', (req, res) => {
+//     res.send('Product router');
+// });
 
-router.get('/list-product', ProductController.getListProduct);
+// router.get('/list-product', ProductController.getListProduct);
 
-router.get('/get-product/:_id', ProductController.getProduct);
+// router.get('/get-product/:_id', ProductController.getProduct);
 
-router.post('/create-student', ProductController.createProduct);
+// router.post('/create-student', ProductController.createProduct);
 
-router.put('/update-product/:_id', ProductController.updateProduct);
+// router.put('/update-product/:_id', ProductController.updateProduct);
 
-router.delete('/delete-product/:_id', ProductController.deleteProduct);
+// router.delete('/delete-product/:_id', ProductController.deleteProduct);
 
-module.exports = router;
+// module.exports = router;
